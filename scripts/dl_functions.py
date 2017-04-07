5a5fc96793c4d458d75ffbce7dbc3c2cb5faff32
Carles Poles-Mielgo <carles.poles@gmail.com>
2017-04-06 22:07:13 -0700

    check project

---
+++ scripts/dl_functions.py
@@ -0,0 +1,214 @@
+import pandas as pd
+import numpy as np
+import os
+import glob
+import IPython
+import keras
+import matplotlib.pyplot as plt
+import pandas as pd
+from IPython.display import display
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential, model_from_json
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+from keras.utils import np_utils
+from keras.preprocessing.image import array_to_img, img_to_array
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from skimage import io, color, exposure, transform
+from sklearn.metrics import confusion_matrix, roc_curve
+from sklearn.cross_validation import train_test_split
+
+
+class TrainingVisualizerCallback(keras.callbacks.History):
+    """
+    A callback is used to displays training information after every epoch.
+    """
+    def on_epoch_end(self, epoch, logs={}):
+        super().on_epoch_end(epoch, logs)
+        IPython.display.clear_output(wait=True)
+        pd.DataFrame(self.history).plot()
+        plt.show()
+
+
+def preprocess_img(img, IMG_SIZE):
+    # Histogram normalization in y
+    hsv = color.rgb2hsv(img)
+    hsv[:, :, 2] = exposure.equalize_hist(hsv[:, :, 2])
+    img = color.hsv2rgb(hsv)
+
+    # central scrop
+    min_side = min(img.shape[:-1])
+    centre = img.shape[0]//2, img.shape[1]//2
+    img = img[centre[0]-min_side//2:centre[0]+min_side//2,
+              centre[1]-min_side//2:centre[1]+min_side//2,
+              :]
+
+    # rescale to standard size
+    img = transform.resize(img, (IMG_SIZE, IMG_SIZE))
+
+    # roll color axis to axis 0
+    img = np.rollaxis(img, -1)
+
+    return img
+
+
+def create_images_array(image_path, IMG_SIZE):
+    imgs = []
+    dirPath = image_path
+    fileList = os.listdir(dirPath)
+    for fileName in fileList:
+        img = preprocess_img(io.imread(dirPath + '/' + fileName), IMG_SIZE)
+        imgs.append(img)
+
+    return np.array(imgs, dtype='float32')
+
+
+def cnn_model_baseline(IMG_SIZE):
+    global NUM_CLASSES
+    NUM_CLASSES = 2
+    model = Sequential()
+    model.add(Convolution2D(32, (3, 3), padding='same', input_shape=(3, IMG_SIZE, IMG_SIZE), activation='relu'))
+    model.add(Convolution2D(32, (3, 3), activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Convolution2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Convolution2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+
+    return model
+
+
+def show_confusion_matrix(C, class_labels=['0', '1']):
+    """
+    C: ndarray, shape (2,2) as given by scikit-learn confusion_matrix function
+    class_labels: list of strings, default simply labels 0 and 1.
+    Draws confusion matrix with associated metrics.
+
+    Minimum required imports:
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    Source: http://notmatthancock.github.io/2015/10/28/confusion-matrix.html
+
+    """
+    assert C.shape == (2, 2), "Confusion matrix should be from binary classification only."
+
+    # true negative, false positive, etc...
+    tn = C[0, 0]; fp = C[0,1]; fn = C[1,0]; tp = C[1,1];
+
+    NP = fn+tp # Num positive examples
+    NN = tn+fp # Num negative examples
+    N = NP+NN
+
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.add_subplot(111)
+    ax.imshow(C, interpolation='nearest', cmap=plt.cm.gray)
+
+    # Draw the grid boxes
+    ax.set_xlim(-0.5, 2.5)
+    ax.set_ylim(2.5, -0.5)
+    ax.plot([-0.5, 2.5], [0.5, 0.5], '-k', lw=2)
+    ax.plot([-0.5, 2.5], [1.5, 1.5], '-k', lw=2)
+    ax.plot([0.5, 0.5], [-0.5, 2.5], '-k', lw=2)
+    ax.plot([1.5, 1.5], [-0.5, 2.5], '-k', lw=2)
+
+    # Set xlabels
+    ax.set_xlabel('Predicted Label', fontsize=16)
+    ax.set_xticks([0, 1, 2])
+    ax.set_xticklabels(class_labels + [''])
+    ax.xaxis.set_label_position('top')
+    ax.xaxis.tick_top()
+    # These coordinate might require some tinkering. Ditto for y, below.
+    ax.xaxis.set_label_coords(0.34, 1.06)
+
+    # Set ylabels
+    ax.set_ylabel('True Label', fontsize=16, rotation=90)
+    ax.set_yticklabels(class_labels + [''], rotation=90)
+    ax.set_yticks([0, 1, 2])
+    ax.yaxis.set_label_coords(-0.09, 0.65)
+
+    # Fill in initial metrics: tp, tn, etc...
+    ax.text(0, 0,
+            'True Neg: %d\n(Num Neg: %d)'%(tn,NN),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w', boxstyle='round,pad=1'))
+
+    ax.text(0, 1,
+            'False Neg: %d'%fn,
+            va='center',
+            ha='center',
+            bbox=dict(fc='w', boxstyle='round,pad=1'))
+
+    ax.text(1, 0,
+            'False Pos: %d'%fp,
+            va='center',
+            ha='center',
+            bbox=dict(fc='w', boxstyle='round,pad=1'))
+
+    ax.text(1, 1,
+            'True Pos: %d\n(Num Pos: %d)'%(tp, NP),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w', boxstyle='round,pad=1'))
+
+    # Fill in secondary metrics: accuracy, true pos rate, etc...
+    ax.text(2, 0,
+            'False Pos Rate: %.2f'%(fp / (fp+tn+0.)),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w', boxstyle='round,pad=1'))
+
+    ax.text(2, 1,
+            'True Pos Rate: %.2f'%(tp / (tp+fn+0.)),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w', boxstyle='round,pad=1'))
+
+    ax.text(2, 2,
+            'Accuracy: %.2f'%((tp+tn+0.)/N),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w',boxstyle='round,pad=1'))
+
+    ax.text(0, 2,
+            'Neg Pre Val: %.2f'%(1-fn/(fn+tn+0.)),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w', boxstyle='round,pad=1'))
+
+    ax.text(1, 2,
+            'Pos Pred Val: %.2f'%(tp/(tp+fp+0.)),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w', boxstyle='round,pad=1'))
+
+    plt.tight_layout()
+    plt.show();
+
+
+def plot_roc(y_test, y_prediction, model_label, plot_title):
+    """
+    The function takes as parameters the test labels (observations),
+    the test predictions and two strings to customize the plot: the
+    name of the model, and a title for the plot.
+    It relies on sklearn.metrics.roc_curve to extract fpr and tpr
+    to make the plot.
+    IMPORTANT: the predictions must be probabilities in order to plot
+    the curve: not .predict(X_test), but .predict_proba(X_test)
+    """
+    fpr, tpr, thresholds = roc_curve(y_test, y_prediction)
+    plt.plot(fpr, tpr, label=model_label)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel("False Positive Rate (1 - Specificity)")
+    plt.ylabel("True Positive Rate (Sensitivity)")
+    plt.plot([0, 1], [0, 1], 'k--', color="red", label="Random guess")
+    plt.legend(loc='best')
+    plt.grid()
+    plt.title("ROC Curve - " + plot_title + "");
