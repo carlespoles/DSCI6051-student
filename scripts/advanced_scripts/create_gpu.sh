ceabcf5ecf7a3de78ad98b5cfb6d587724b7dc2f
Carles Poles-Mielgo <carles.poles@gmail.com>
2017-04-29 13:37:41 -0700

    check script

---
+++ scripts/advanced_scripts/create_gpu.sh
@@ -0,0 +1,11 @@
+gcloud beta compute instances create \
+gpu-sspyder --machine-type n1-standard-2 \
+--zone us-east1-d \
+--accelerator type=nvidia-tesla-k80,count=1 \
+--image-family ubuntu-1604-lts \
+--image-project ubuntu-os-cloud \
+--scopes bigquery,datastore,logging-write,storage-full,https://www.googleapis.com/auth/pubsub \
+--boot-disk-size 200GB \
+--maintenance-policy TERMINATE \
+--restart-on-failure \
+--metadata-from-file startup-script=docker.sh
